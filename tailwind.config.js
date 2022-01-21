6803fb1a755cad2723d8d5758874ef3d1dd32183
mohammad tahourian <mhdtahourian@gmail.com>
2022-01-21 10:53:49 +0330

    config top menu and start top bar

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,16 @@
 module.exports = {
     content: ["./src/**/*.{html,js}"],
     theme: {
+        container: {
+            center: true,
+            padding: '1rem',
+            screens: {
+                sm: '600px',
+                md: '728px',
+                lg: '984px',
+                xl: '1240px',
+            },
+        },
         extend: {},
     },
     plugins: [],
